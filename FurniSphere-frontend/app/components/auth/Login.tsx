41ed74145b30ac05add1b5c14fd3ab8e8faeba8f
Mohammad Osseily <mohammad.osseily2@gmail.com>
2026-01-15 13:01:38 +0200

    fixing deployment error

--- FurniSphere-frontend/app/components/auth/Login.tsx
+++ FurniSphere-frontend/app/components/auth/Login.tsx
@@ -124,7 +124,7 @@ const Login: React.FC = () => {
 
         <div className="text-center">
           <p className="text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/register"
               className="text-primary hover:underline font-medium"
